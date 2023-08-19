6a2288db2651444afdea2ca7ca5393da93a50abc
meetrachhadiya <meetrachhadiya8331@gmail.com>
2023-08-19 21:01:19 +0530

    "Initialize"

---
+++ script.js
@@ -0,0 +1,9 @@
+// Load library from the CDN
+var element = document.getElementById("element");
+
+    // Setup and start animation!
+    
+        var typed = new Typed(element, {
+            strings: ['Web Developer', 'Web Designer', 'Java Programmer.'],
+            typeSpeed: 50,
+        });
